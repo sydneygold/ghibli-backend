e572fa0fbc972959352813eabb19c260082d713b
haileyringier <haileyringier@outlook.com>
2020-09-01 13:09:29 -0600

    adds controllers

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,2 +1,14 @@
 class SpeciesController < ApplicationController
+    def index 
+        @species = Species.all
+        
+        render json: @species
+    end
+
+    def show
+        @species = Species.find(params[:id])
+        
+        render json: @species
+    end
+
 end
