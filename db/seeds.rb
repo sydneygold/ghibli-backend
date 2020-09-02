6c9d8000fe406eddc4b978a521a2624e6827bea5
haileyringier <haileyringier@outlook.com>
2020-09-01 20:38:05 -0600

    fixes formating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# require 'rest-client'
-
-# species = RestClient.get "https://ghibliapi.herokuapp.com/species"
-# species_array = JSON.parse(species)
-
-# species_array.each do |s|
-#     Species.create(
-#         name: s["name"],
-#         classification: s["classification"],
-#         eye_colors: s["eye_colors"],
-#         hair_colors: s["hair_colors"]
-#     )
-# end
-# require 'pry'
 
 Species.destroy_all
 Movie.destroy_all 
 
-spirited_away = Movie.create(
+Movie.create(
     name: "Spirited Away",
     image: "https://www.spoon-tamago.com/wp-content/uploads/2019/06/spirited-away-china-poster-1-1.jpg"
 )
-totoro = Movie.create(
+Movie.create(
     name: "My Neighbor Totoro",
     image: "https://fesapusewebsite.blob.core.windows.net/fathom/totoro3-9b8a40ca66146ccbae1519b9b88670f4.jpg"
 )
-mononoke = Movie.create(
+Movie.create(
     name: "Princess Mononoke",
     image: "https://images-na.ssl-images-amazon.com/images/I/51Xl0K7PlUL._AC_.jpg"
 )
-howls = Movie.create(
+Movie.create(
     name: "Howl's Moving Castle",
     image: "https://images-na.ssl-images-amazon.com/images/I/8163kSmfdvL._AC_SL1500_.jpg"
 )
-kikis = Movie.create(
+Movie.create(
     name: "Kiki's Delivery Service",
     image: "https://images-na.ssl-images-amazon.com/images/I/51yPFJAL9iL._AC_.jpg"
 )
-ponyo = Movie.create(
+Movie.create(
     name: "Ponyo",
     image: "https://cdn.shopify.com/s/files/1/0153/2051/products/Tracie_Ching_-_Ponyo_Foil_754x.jpg?v=1580086168"
 )
-# binding.pry 
-# 0
 
 Species.create(
     name: "human",
